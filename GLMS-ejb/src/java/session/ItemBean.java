04e21314c9ae10ddda644d1197cdc0773865807f
Philson <Philson@Philson_Zenbook.gateway.2wire.net>
2014-03-11 20:11:02 +0800

    Backend for items is done!! :)

--- GLMS-ejb/src/java/session/ItemBean.java
+++ GLMS-ejb/src/java/session/ItemBean.java
@@ -3,11 +3,18 @@
  */
 package session;
 
+import entity.Booster;
 import entity.Item;
+import entity.Key;
+import entity.Module;
+import entity.Pet;
+import entity.Quest;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -17,12 +24,12 @@ import javax.persistence.PersistenceContext;
 public class ItemBean implements ItemBeanLocal
 {
     //Functionalities needed:        
-    //  1. Retrieve all the Items for a particular module      [NOT DONE]
+    //  1. Retrieve all the Items for a particular module      [DONE!]
     //  2. Create a Style Item                                 [NOT DONE]
-    //  3. Create a Pet                                        [NOT DONE]
-    //  4. Create a Booster Pack                               [NOT DONE]
-    //  5. Create a Quest Item                                 [NOT DONE]
-    //  6. Delete an Item                                      [NOT DONE]
+    //  3. Create a Pet                                        [DONE!]
+    //  4. Create a Booster Pack                               [DONE!]
+    //  5. Create a Quest Item                                 [DONE!]
+    //  6. Delete an Item                                      [DONE!]
 
     @PersistenceContext
     private EntityManager em;
@@ -30,23 +37,145 @@ public class ItemBean implements ItemBeanLocal
     //Retrieve all the Items for a particular module
     public List<Item> getAllItems(String moduleId) throws Exception
     {
-//        em.flush();
-//
-//        ET_Administrator admin = getAdmin(userID);
-//        List<ET_Coupon> allCpn = new ArrayList(admin.getCoupon());
-//        List<ET_Coupon> savedCpns = new ArrayList();
-//
-//        for (ET_Coupon cpn : allCpn)
-//        {
-//            if (cpn.getCouponState().equals("saved"))
-//            {
-//                savedCpns.add(cpn);
-//                System.out.println(cpn + "added to saved list");
-//            }
-//        }
-//
-//        return savedCpns;
-        return null;
+        em.flush();
+
+        Query query = em.createQuery("SELECT i FROM Item i");
+        List<Item> allItems = new ArrayList(query.getResultList());
+        List<Item> moduleItems = new ArrayList();
+
+        for (Item item : allItems)
+        {
+            if (item.getModule().getModuleId().equals(moduleId) && !item.getStatus().equalsIgnoreCase("DELETED"))
+            {
+                moduleItems.add(item);
+                System.out.println(item + "added to moduleItems list");
+            }
+        }
+        return moduleItems;
+    }
+
+    //This method creates a new Pet Item for a Module
+    public void createPet(String moduleId, String name, String description, int cost, int bonus) throws Exception
+    {
+        Module module = getModule(moduleId);
+        String imgURL = "Need to upload the image";
+        Pet pet = new Pet();
+
+        //Set all the item info
+        pet.setName(name);
+        pet.setDescription(description);
+        pet.setCost(cost);
+        pet.setMaxBonus(bonus);
+        pet.setModule(module);
+        pet.setImgURL(imgURL);
+        pet.setStatus("AVAILABLE");
+
+        //Add the Pet to appropriate Module and persist
+        module.getItems().add(pet);
+        em.persist(pet);
+        em.persist(module);
+        em.flush();
+    }
+
+    //This method creates a new Booster Item for a Module
+    public void createBooster(String moduleId, String name, String description, String type, int cost, int bonus) throws Exception
+    {
+        Module module = getModule(moduleId);
+        String imgURL = "Need to upload the image";
+        String fileURL = "Need the URL of the file uploaded";
+        Booster booster = new Booster();
+
+        //Set all the item info
+        booster.setName(name);
+        booster.setDescription(description);
+        booster.setCost(cost);
+        booster.setModule(module);
+        booster.setImgURL(imgURL);
+        booster.setFileURL(fileURL);
+        booster.setType(type);
+        booster.setStatus("AVAILABLE");        
+
+        //Add the Booster to appropriate Module and persist
+        module.getItems().add(booster);
+        em.persist(booster);
+        em.persist(module);
+        em.flush();
+    }
+
+    //This method creates a new Key Item for a particular Quest in a Module
+    public void createKey(String moduleId, Long questId, String name, String description, String type, int cost, int bonus) throws Exception
+    {
+        Module module = getModule(moduleId);
+        Quest quest = getQuest(questId);
+        String imgURL = "Need to upload the image";
+        Key key = new Key();
+
+        //Set all the item info
+        key.setName(name);
+        key.setDescription(description);
+        key.setCost(cost);
+        key.setImgURL(imgURL);
+        key.setModule(module);
+        key.setUnlock(quest);
+        key.setStatus("AVAILABLE");
+
+        //Add the Key to appropriate Module & Quest and persist
+        module.getItems().add(key);
+        quest.setKeyRequired(key);
+        em.persist(key);
+        em.persist(quest);
+        em.persist(module);
+        em.flush();
+    }    
+
+    //This method is used to set an item as DELETED
+    public void deleteItem(Long itemId) throws Exception
+    {
+        Item item = getItem(itemId);
+        item.setStatus("DELETED");
+        em.persist(item);
+        em.flush();
+    } 
+    
+    
+    
+    
+    
+    
+    //Helper method: retrieves the module based on moduleId
+    private Module getModule(String moduleId) throws Exception
+    {
+        Module module = em.find(Module.class, moduleId);
+        //Check if Module exists
+        if (module == null)
+        {
+            throw new Exception("ERROR: MODULE DOES NOT EXIST!");
+        }
+        return module;
+    }
+    
+    //Helper method: retrieves the quest based on questId
+    private Quest getQuest(Long questId) throws Exception
+    {
+        Quest quest = em.find(Quest.class, questId);
+        //Check if Quest exists
+        if (quest == null)
+        {
+            throw new Exception("ERROR: QUEST DOES NOT EXIST!");
+        }
+        return quest;
+    }
+    
+    //Helper method: retrieves the item based on itemId
+    public Item getItem(Long itemId) throws Exception
+    {
+        Item item = em.find(Item.class, itemId);
+        //Check if Item exists
+        if (item == null || item.getStatus().equalsIgnoreCase("DELETED"))
+        {
+            throw new Exception("ERROR: ITEM DOES NOT EXIST!");
+        }
+        return item;
     }
 
 }
--- GLMS-ejb/src/java/session/ItemBeanLocal.java
+++ GLMS-ejb/src/java/session/ItemBeanLocal.java
@@ -6,6 +6,8 @@
 
 package session;
 
+import entity.Item;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -15,5 +17,17 @@ import javax.ejb.Local;
 @Local
 public interface ItemBeanLocal
 {
+
+    public Item getItem(Long itemId) throws Exception;
+
+    public void deleteItem(Long itemId) throws Exception;
+
+    public void createKey(String moduleId, Long questId, String name, String description, String type, int cost, int bonus) throws Exception;
+
+    public void createBooster(String moduleId, String name, String description, String type, int cost, int bonus) throws Exception;
+
+    public void createPet(String moduleId, String name, String description, int cost, int bonus) throws Exception;
+
+    public List<Item> getAllItems(String moduleId) throws Exception;
     
 }
